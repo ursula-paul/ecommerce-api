846c36b61a90b79747418391fae2c659bd989b1e
ursula Paul <ursulapaul31@gmail.com>
2023-01-10 17:11:52 +0100

    worked on auth and user.jsi

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+mongoose.set('strictQuery', false);
+const dotenv = require("dotenv")
 const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");
-const stripeRoute = require("./routes/stripe");
-const cors = require("cors");
 
-dotenv.config();
+dotenv.config()
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection is Successfull!"))
+    .catch((err) => {
+        console.log(err)
+    })
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
+PORT = process.env.PORT
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
-});
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`server is running on PORT ${PORT}`)
+})
+
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+
+// const authRoute = require("./routes/auth");
+// const productRoute = require("./routes/product");
+// const cartRoute = require("./routes/cart");
+// const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
+// const cors = require("cors");
+
+// dotenv.config();
+
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(() => console.log("DB Connection Successfull!"))
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/carts", cartRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
+
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log("Backend server is running!");
+// });
+

c74bcca0fcf3fcf0c05a1aa070b01ee17cb98449
ursula Paul <ursulapaul31@gmail.com>
2023-01-11 08:36:57 +0100

    completed the user model

--- routes/user.js
+++ routes/user.js
@@ -32,63 +32,63 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
 //DELETE
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try {
+try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted...");
-  } catch (err) {
+} catch (err) {
     res.status(500).json(err);
   }
 });
 
 //GET USER
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
-  try {
+try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
     res.status(200).json(others);
-  } catch (err) {
+} catch (err) {
     res.status(500).json(err);
-  }
+}
 });
 
 //GET ALL USER
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
-  const query = req.query.new;
-  try {
+const query = req.query.new;
+try {
     const users = query
-      ? await User.find().sort({ _id: -1 }).limit(5)
-      : await User.find();
+    ? await User.find().sort({ _id: -1 }).limit(5)
+    : await User.find();
     res.status(200).json(users);
-  } catch (err) {
+} catch (err) {
     res.status(500).json(err);
-  }
+}
 });
 
 //GET USER STATS
 
 router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-  const date = new Date();
-  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+const date = new Date();
+const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
-  try {
+try {
     const data = await User.aggregate([
-      { $match: { createdAt: { $gte: lastYear } } },
-      {
+    { $match: { createdAt: { $gte: lastYear } } },
+    {
         $project: {
-          month: { $month: "$createdAt" },
+        month: { $month: "$createdAt" },
         },
-      },
-      {
+    },
+    {
         $group: {
-          _id: "$month",
-          total: { $sum: 1 },
+        _id: "$month",
+        total: { $sum: 1 },
         },
-      },
+    },
     ]);
     res.status(200).json(data)
-  } catch (err) {
+} catch (err) {
     res.status(500).json(err);
-  }
+}
 });
 
 module.exports = router;
